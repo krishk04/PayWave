f352d84f7502d6da066b54c23ef3e5c8f5da6fbb
krishk04 <krishk0402@gmail.com>
2024-07-03 23:19:03 +0530

    added cors

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const app=express();
 const PORT=3000;
+const cors=require("cors");
+const bodyParser=require("body-parser");
+app.use(cors());
+app.use(express.json());
 const {router}=require("./routes/index.js");
 app.use("/api/v1",router);
 
-app.listen(PORT,()=>{
+
+app.listen(3000,()=>{
     console.log(`server running on port ${PORT}`)
 });
 
